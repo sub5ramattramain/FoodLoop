050a81d89f3e9c3190718f0416420582dec7a910
sub5ramattramain <fpoparosu@gmail.com>
2026-05-02 22:58:15 +0300

    adaugat un navbar placeholder

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,22 +1,31 @@
-import { NavLink } from 'react-router';
-import './Navbar.css'; 
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import styles from './Navbar.module.css';
 
 function Navbar() {
-  return (
-    <nav className="navbar-container">
-      <div className="nav-zone">
-        <NavLink to="/about" className="nav-link">About</NavLink>
-      </div>
+    const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
 
-      <div className="nav-zone nav-center">
-        <NavLink to="/" className="nav-logo">FoodLoop</NavLink>
-      </div>
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logoContainer}>
+                <span className={styles.logoText}>FoodLoop</span>
+            </div>
 
-      <div className="nav-zone nav-right">
-        <NavLink to="/login" className="nav-link">Login</NavLink>
-      </div>
-    </nav>
-  );
+            <div className={styles.navLinks}>
+                <a href="#oferte" className={styles.link}>Oferte</a>
+                <a href="#cum-functioneaza" className={styles.link}>Cum functioneaza</a>
+                <a href="#despre_noi" className={styles.link}>Despre noi</a>
+            </div>
+
+            <div className={styles.authButtons}>
+                {!isAuthenticated ? (
+                    <button className={styles.loginBtn} onClick={() => loginWithRedirect()}>Login</button>
+                ) : (
+                    <button className={styles.logoutBtn} onClick={() => logout({ returnTo: window.location.origin })}>Logout</button>
+                )}
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar;
